bf81201485fd592d606eef8f8576ec9891bc8ae6
alesyagdh <alesyagdh@gmail.com>
2026-09-24 19:57:16 +0300

    added solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,27 +1,32 @@
+from pytest_mock import Mocker
 from app.main import can_access_google_page
 
-def test_can_access_google_page_valid_url_and_connection(mocker):
-    mocker.patch('app.main.valid_google_url', return_value=True)
-    mocker.patch('app.main.has_internet_connection', return_value=True)
+
+def test_can_access_google_page_valid_url_and_connection(
+        mocker: Mocker) -> None:
+    mocker.patch("app.main.valid_google_url", return_value=True)
+    mocker.patch("app.main.has_internet_connection", return_value=True)
     result = can_access_google_page("https://google.com")
     assert result == "Accessible"
 
-def test_can_access_google_page_invalid_url(mocker):
-    mocker.patch('app.main.valid_google_url', return_value=False)
-    mocker.patch('app.main.has_internet_connection', return_value=True)
-    result = can_access_google_page("https://google.com")
-    assert result == "Not accessible"
 
-def test_can_access_google_page_no_internet_connection(mocker):
-    mocker.patch('app.main.valid_google_url', return_value=True)
-    mocker.patch('app.main.has_internet_connection', return_value=False)
-    result = can_access_google_page("https://google.com")
-    assert result == "Not accessible"
-
-def test_can_access_google_page_invalid_url_and_no_connection(mocker):
-    mocker.patch('app.main.valid_google_url', return_value=False)
-    mocker.patch('app.main.has_internet_connection', return_value=False)
+def test_can_access_google_page_invalid_url(mocker: Mocker) -> None:
+    mocker.patch("app.main.valid_google_url", return_value=False)
+    mocker.patch("app.main.has_internet_connection", return_value=True)
     result = can_access_google_page("https://google.com")
     assert result == "Not accessible"
 
 
+def test_can_access_google_page_no_internet_connection(mocker: Mocker) -> None:
+    mocker.patch("app.main.valid_google_url", return_value=True)
+    mocker.patch("app.main.has_internet_connection", return_value=False)
+    result = can_access_google_page("https://google.com")
+    assert result == "Not accessible"
+
+
+def test_can_access_google_page_invalid_url_and_no_connection(
+        mocker: Mocker) -> None:
+    mocker.patch("app.main.valid_google_url", return_value=False)
+    mocker.patch("app.main.has_internet_connection", return_value=False)
+    result = can_access_google_page("https://google.com")
+    assert result == "Not accessible"
